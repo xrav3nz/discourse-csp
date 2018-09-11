dc09e0a2b7bd32b0f0f1517849cd9afd5f633496
Kyle Zhao <kzhao.sw@gmail.com>
2018-09-11 11:48:06 +0800

    Accept and log CSP violations

--- plugin.rb
+++ plugin.rb
@@ -1,4 +1,4 @@
-# name: DiscourseCSP
+# name: discourse-csp
 # about:
 # version: 0.1
 # authors: xrav3nz
@@ -6,13 +6,13 @@
 
 enabled_site_setting :discourse_csp_enabled
 
-PLUGIN_NAME ||= "DiscourseCSP".freeze
+PLUGIN_NAME ||= "DiscourseCsp".freeze
 
 after_initialize do
-  module ::DiscourseCSP
+  module ::DiscourseCsp
     class Engine < ::Rails::Engine
       engine_name PLUGIN_NAME
-      isolate_namespace DiscourseCSP
+      isolate_namespace DiscourseCsp
     end
   end
 
@@ -21,7 +21,30 @@ after_initialize do
     after_action :set_csp_header
 
     def set_csp_header
-      response.headers["Content-Security-Policy-Report-Only"] = "script-src 'self' 'unsafe-eval';"
+      response.headers["Content-Security-Policy-Report-Only"] = "script-src 'self' 'unsafe-eval'; report-uri /discourse-csp/reports"
     end
+  end
+
+  class DiscourseCsp::ReportsController < ::ApplicationController
+    requires_plugin PLUGIN_NAME
+
+    skip_before_action :check_xhr, :preload_json, :verify_authenticity_token, only: [:create]
+
+    def create
+      report = JSON.parse(request.body.read)['csp-report']
+
+      Logster.add_to_env(request.env, 'CSP Report', report)
+      Rails.logger.error("CSP Violation: '#{report['blocked-uri']}'")
+
+      head :ok
+    end
+  end
+
+  DiscourseCsp::Engine.routes.draw do
+    resources :reports, only: [:create]
+  end
+
+  Discourse::Application.routes.append do
+    mount ::DiscourseCsp::Engine, at: 'discourse-csp'
   end
 end
